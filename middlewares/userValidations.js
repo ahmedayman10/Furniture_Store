28ff20a78fd280759937664d2ef12e96ca8a0a34
ahmedayman10 <86632245+ahmedayman10@users.noreply.github.com>
2022-04-06 00:22:52 +0200

    Update userValidations.js

--- middlewares/userValidations.js
+++ middlewares/userValidations.js
@@ -1,10 +1,10 @@
 const Joi = require("joi");
 
 const schema = Joi.object({
-  name: Joi.string().min(3).max(15).required(),
+  name: Joi.string().min(3).max(50).required(),
   email: Joi.string().email().required(),
   password: Joi.string().min(5).max(15).required(),
-  phone: Joi.number().min(11).required(),
+  phone: Joi.string().min(11).required(),
   city: Joi.string(),
   street:Joi.string(),
   apartment:Joi.string(),
