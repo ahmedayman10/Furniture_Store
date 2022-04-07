b29a1670e9f6adea353b6c7026b8c36adab7d555
ahmedayman10 <86632245+ahmedayman10@users.noreply.github.com>
2022-04-08 01:55:38 +0200

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -4,8 +4,9 @@ const { Product } = require("../models/product");
 const { Category } = require("../models/category");
 const cloudinary = require("../utils/cloudinaryConfig");
 const multer = require("../utils/multer");
-const customeError = require('../errorHandling/Custom_error');
-const {User} = require('../models/user');
+const customeError = require("../errorHandling/Custom_error");
+const { User } = require("../models/user");
+const mongoose = require("mongoose");
 
 const {
   verifyToken,
@@ -14,46 +15,91 @@ const {
 } = require("../middlewares/verifyToken");
 const res = require("express/lib/response");
 
+router.post(
+  "/",
+  verifyTokenAndAdmin,
+  multer.single("image"),
+  async (req, res, next) => {
+    try {
+      const category = await Category.findById(req.body.category);
+      if (!category) return res.status(400).send("invalid category");
+      //console.log(req.file);
+      const result = await cloudinary.uploader.upload(req.file.path);
+      const product = new Product({
+        name: req.body.name,
+        description: req.body.description,
+        category: category,
+        price: req.body.price,
+        countInStock: req.body.countInStock,
+        isFeatured: req.body.isFeatured,
+        image: result.url,
+        brand: req.body.brand,
+      });
+      //res.json(result);
+      await product.save();
+      // res.status(200).json({product:product});
+      res.send(product);
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  }
+);
 
-router.post("/",verifyTokenAndAdmin, multer.single("image"), async (req, res, next) => {
-  try {
-    const category = await Category.findById(req.body.category);
-    if (!category) return res.status(400).send("invalid category");
-    const result = await cloudinary.uploader.upload(req.file.path);
+router.post("/favourite", async (req, res) => {
+  const {
+    body: { productId, userId },
+  } = req;
+  const productDetails = await Product.findById(productId);
+  let product = await User.updateOne(
+    { _id: userId },
+    { $push: { favouriteList: productId } }
+  );
 
-    const product = new Product({
-      name: req.body.name,
-      description: req.body.description,
-      category:category,
-      price: req.body.price,
-      countInStock: req.body.countInStock,
-      isFeatured: req.body.isFeatured,
-      image:result.url,
-      brand:req.body.brand
-    });
-    await product.save();
-    res.send(product)
-  } catch (err) {
-    res.status(500).send(err);
+  res
+    .status(200)
+    .send(productDetails);
+});
+
+router.get('/favourite',async(req,res)=>{
+  const {userId} = req.body;
+  try{
+    const favProduct = await Product.find({userId})
+    res.status(200).send(favProduct);
+  }catch(err){
+    res.status(404).send(err.message);
   }
 });
 
-router.post('/favourite',async(req,res)=>{
-  const {body:{productId, userId}} = req;
-  let product = await User.updateOne({_id:userId}, {$push:{favouriteList: productId}});
+router.delete('/favourite',async(req,res)=>{
+  try{
+    const {userId, productId} = req.body;
+    if(!mongoose.isValidObjectId(userId) || !mongoose.isValidObjectId(productId)){
+      return res.status(404).send('invalid id');
+    }
 
-  res.status(200).send("Favourite list updated successfully");
+    const user = await User.findById(userId);
+    
+    const product = await Product.findById(productId);
+    
+    const favList = user.favouriteList;
+    
+    const newFavList = favList.filter(item=> item != productId);
+    const updatedUser = await User.findByIdAndUpdate(userId , {favouriteList:newFavList})
+    res.status(200).send(updatedUser)
+  }catch(err){
+    res.status(500).send(err.message);
+  }
+});
 
-})
 
 router.get("/", async (req, res, next) => {
   let { page, limit, categoryId } = req.query;
   const size = await Product.count().exec();
   const skip = (page || 1 - 1) * (limit || 10);
   const pages = Math.ceil(+size / +(limit || 10));
-  try{
+  try {
     if (categoryId) {
-      const myCategory = categoryId.split(',');
+      const myCategory = categoryId.split(",");
       const searchedProducts = await Product.find({ category: myCategory })
         .populate("category")
         .limit(limit)
@@ -67,18 +113,18 @@ router.get("/", async (req, res, next) => {
         .skip(skip)
         .exec();
       if (size == 0)
-        return next(customeError({ status: 400, message: "Products not found" }));
+        return next(
+          customeError({ status: 400, message: "Products not found" })
+        );
       res.send(products);
     }
-  
-  }catch(err){
+  } catch (err) {
     res.status(404).send(err.message);
   }
   //return res.status(200).json({ success: true, products, pages, size });
 });
 
-
-router.get("/:id", async (req, res, next) => {
+router.get("/:id", verifyTokenAndAdmin, async (req, res, next) => {
   try {
     const product = await Product.findById(req.params.id).populate("category");
     res.status(200).send(product);
@@ -87,26 +133,19 @@ router.get("/:id", async (req, res, next) => {
   }
 });
 
-router.get('/category/:id', async(req, res)=>{
- const categoryId = await Category.findById(req.params.id);
- if(!categoryId)return res.status(404).json({success:false, message:'not found'})
-
-  const product = await Product.find({category: categoryId});
-  res.status(200).send(product);
-});
-
-//get product count
-router.get(`/get/count`, async (req, res) => {
-  let productList = await Product.find();
- res.status(200).json({success:true, productsCount:productList.length});
-});
-
 router.get("/get/featured/:count", async (req, res) => {
   const { count } = req.params;
   console.log(count);
   const product = await Product.find({ isFeatured: true }).limit(count);
   res.send(product);
 });
+
+//get product count
+router.get(`/get/count`, async (req, res) => {
+  let productList = await Product.find();
+  res.status(200).json({ success: true, productsCount: productList.length });
+});
+
 
 
 
@@ -118,8 +157,10 @@ router.patch("/:id",verifyTokenAndAdmin, multer.single("image"), async (req, res
   if(req.file){
     const result = await cloudinary.uploader.upload(req.file.path);
     imgURL = result.url;
-    
+    console.log(imgURL);
+    console.log("asfafafas");
   }
+  console.log(imgURL);
   try{
       const updatedProduct = await Product.findByIdAndUpdate(req.params.id, {
       name: req.body.name,
@@ -137,8 +178,6 @@ router.patch("/:id",verifyTokenAndAdmin, multer.single("image"), async (req, res
     res.send(err);
   }
 });
-
-
 
 
 router.delete("/:id", verifyTokenAndAdmin, async (req, res, next) => {
