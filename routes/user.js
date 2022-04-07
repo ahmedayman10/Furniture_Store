42bc2ccae435bc6cd20dbb8ac1c55eeffef10dfa
ahmedayman10 <86632245+ahmedayman10@users.noreply.github.com>
2022-04-07 02:16:53 +0200

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -30,20 +30,20 @@ router.delete("/:id",verifyTokenAndAuthorization,async(req, res,next)=>{
   const user = await User.findByIdAndRemove(id);
   res.status(200).json({user:user});
 })
-
-router.patch("/:id", verifyTokenAndAuthorization,async (req, res, next) => {
-  const { id } = req.params;
-    const updatedUser = await User.findByIdAndUpdate(
-      id ,
-      {
-        $set: req.body,
-      },
-      { new: true }
-    );
-    const salt = await bcrypt.genSalt(10);
-    updatedUser.password = await bcrypt.hash(updatedUser.password, salt);
-
-    res.status(200).send(updatedUser);
+router.patch("/:id", verifyTokenAndAuthorization, async (req, res, next) => {
+  let { id } = req.params;
+  const user = req.body;
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
+  const updatedUser = await User.findByIdAndUpdate(
+    id,
+    {
+      $set: user,
+    },
+    { new: true }
+  );
+  console.log(updatedUser);
+  res.status(200).send(updatedUser);
 });
 
 module.exports = router;
