8626394b0ac3db861137433e6b0b86c6b24f9660
ahmedayman10 <86632245+ahmedayman10@users.noreply.github.com>
2022-04-08 00:04:52 +0200

    Update category.js

--- routes/category.js
+++ routes/category.js
@@ -47,13 +47,30 @@ router.get('/:id',async(req,res,next)=>{
       
 });
 
-router.patch('/:id',verifyTokenAndAdmin,async(req, res)=>{
-    const updatedCategory = await Category.findByIdAndUpdate(req.params.id,{
-        $set: req.body
-    },{new: true});
-    if(!updatedCategory)return res.status(404).send('category doesnot exists');
-    res.status(200).json({category: updatedCategory});
+
+router.patch('/:id',verifyTokenAndAdmin,multer.single("image"),async(req, res)=>{
+    const category = await Category.findById(req.params.id);
+    let imgURL = category.image;
+    console.log(req.file);
+    if(req.file){
+      const result = await cloudinary.uploader.upload(req.file.path);
+      imgURL = result.url;
+    }
+    try{
+        const updatedCategory = await Category.findByIdAndUpdate(req.params.id, {
+        name: req.body.name,
+        color:req.body.color,
+        image: imgURL,
+        icon: req.body.icon,
+        });
+        await updatedCategory.save();
+        res.send(updatedCategory);
+    }
+        catch(err){
+            res.status(404).json(err.message)
+        }
 });
+
 
 router.delete('/:id',verifyTokenAndAdmin,async(req, res)=>{
     const deletedCategory = await Category.findByIdAndRemove(req.params.id);
