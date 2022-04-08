f0a6246646ce706411c54aa54f687051602f4d16
ahmedayman10 <86632245+ahmedayman10@users.noreply.github.com>
2022-04-08 02:53:11 +0200

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -124,7 +124,7 @@ router.get("/", async (req, res, next) => {
   //return res.status(200).json({ success: true, products, pages, size });
 });
 
-router.get("/:id", verifyTokenAndAdmin, async (req, res, next) => {
+router.get("/:id", async (req, res, next) => {
   try {
     const product = await Product.findById(req.params.id).populate("category");
     res.status(200).send(product);
